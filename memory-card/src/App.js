8192660e04a6fdaa5db671706d86f0a3e706ed62
DanielNicolae <deadlypoet@protonmail.com>
2021-12-14 22:48:21 +0200

    I extracted the values for MyAttack and OpponentsAttack.

--- memory-card/src/App.js
+++ memory-card/src/App.js
@@ -3,26 +3,31 @@ import './App.css';
 import Card from "./components/Card";
 
 function App() {
-  const [cards, setCards] = useState([]);
-
+  const [myAttack, setMyAttack] = useState(0);
+  const [opponentsAttack, setOpponentsAttack] = useState(0);
   useEffect(() => {
-    const insertCardOnTable = (e) => {
-      const myTable = document.getElementsByClassName("MyCardsOnTable")[0];
-      const opponentsTable = document.getElementsByClassName("OpponentsCardsOnTable")[0];
-      const opponentsCards = document.getElementsByClassName("OpponetsCards")[0].children;
-      const randomCardNum = Math.floor(Math.random() * opponentsCards.length);
+    const myCards = document.getElementsByClassName("MyCard");
+    console.log(myCards)
+    const myTable = document.getElementsByClassName("MyCardsOnTable")[0];
+    const opponentsTable = document.getElementsByClassName("OpponentsCardsOnTable")[0];
 
-      if (e.target.className === "CardContainer" ||
-        e.target.className === "CardProperties" ||
-        e.target.className === "CardsLife" ||
-        e.target.className === "CardsAttack" ||
-        e.target.className === "CardName") {
+    const insertCardOnTable = (e) => {
+      const opponentsCards = document.getElementsByClassName("OpponentsCards")[0].children;
+      const randomCardNum = Math.floor(Math.random() * opponentsCards.length);
+      const getOpponetsAttack = opponentsCards[randomCardNum].firstChild.firstChild.children[1].textContent.split('')[6];
+      setOpponentsAttack(getOpponetsAttack);
+      console.log(`OpponentsAttack=${getOpponetsAttack}`);
+      if (e.target.className === "CardContainer") {
         myTable.appendChild(e.target);
         opponentsTable.appendChild(opponentsCards[randomCardNum]);
+        const getMyAttack = e.target.firstChild.children[1].textContent.split('')[6];
+        setMyAttack(getMyAttack);
+        console.log(`MyAttack=${getMyAttack}`);
       }
 
     };
     document.addEventListener("click", insertCardOnTable);
+
 
     return () => {
       document.removeEventListener("click", insertCardOnTable);
@@ -37,103 +42,123 @@ function App() {
         </div>
       </header>
       <main>
-        <div className='OpponetsCards'>
-          <Card
-            cardLife={10}
-            cardAttack={5}
-            enemyAttack={5}
-            effect={0}
-            cardName={"Demo card"}
-            cardImage={""}
-            imageDesc={"Img"}
-          />
-          <Card
-            cardLife={8}
-            cardAttack={4}
-            enemyAttack={2}
-            effect={0}
-            cardName={"Demo card"}
-            cardImage={""}
-            imageDesc={"Img"}
-          />
-          <Card
-            cardLife={12}
-            cardAttack={5}
-            enemyAttack={3}
-            effect={0}
-            cardName={"Demo card"}
-            cardImage={""}
-            imageDesc={"Img"}
-          />
-          <Card
-            cardLife={4}
-            cardAttack={2}
-            enemyAttack={5}
-            effect={0}
-            cardName={"Demo card"}
-            cardImage={""}
-            imageDesc={"Img"}
-          />
-          <Card
-            cardLife={2}
-            cardAttack={1}
-            enemyAttack={1}
-            effect={0}
-            cardName={"Demo card"}
-            cardImage={""}
-            imageDesc={"Img"}
-          />
+        <div className='OpponentsCards'>
+          <div className='OpponentsCard'>
+            <Card
+              cardLife={10}
+              cardAttack={5}
+              enemyAttack={myAttack}
+              effect={0}
+              cardName={"Demo card"}
+              cardImage={""}
+              imageDesc={"Img"}
+            />
+          </div>
+          <div className='OpponentsCard'>
+            <Card
+              cardLife={8}
+              cardAttack={4}
+              enemyAttack={myAttack}
+              effect={0}
+              cardName={"Demo card"}
+              cardImage={""}
+              imageDesc={"Img"}
+            />
+          </div>
+          <div className='OpponentsCard'>
+            <Card
+              cardLife={12}
+              cardAttack={5}
+              enemyAttack={myAttack}
+              effect={0}
+              cardName={"Demo card"}
+              cardImage={""}
+              imageDesc={"Img"}
+            />
+          </div>
+          <div className='OpponentsCard'>
+            <Card
+              cardLife={4}
+              cardAttack={2}
+              enemyAttack={myAttack}
+              effect={0}
+              cardName={"Demo card"}
+              cardImage={""}
+              imageDesc={"Img"}
+            />
+          </div>
+          <div className='OpponentsCard'>
+            <Card
+              cardLife={2}
+              cardAttack={1}
+              enemyAttack={myAttack}
+              effect={0}
+              cardName={"Demo card"}
+              cardImage={""}
+              imageDesc={"Img"}
+            />
+          </div>
         </div>
         <div className='Table'>
           <div className='OpponentsCardsOnTable'></div>
           <div className='MyCardsOnTable'></div>
         </div>
         <div className='MyCards'>
-          <Card
-            cardLife={10}
-            cardAttack={5}
-            enemyAttack={5}
-            effect={0}
-            cardName={"Demo card"}
-            cardImage={""}
-            imageDesc={"Img"}
-          />
-          <Card
-            cardLife={8}
-            cardAttack={4}
-            enemyAttack={2}
-            effect={0}
-            cardName={"Demo card"}
-            cardImage={""}
-            imageDesc={"Img"}
-          />
-          <Card
-            cardLife={12}
-            cardAttack={5}
-            enemyAttack={3}
-            effect={0}
-            cardName={"Demo card"}
-            cardImage={""}
-            imageDesc={"Img"}
-          />
-          <Card
-            cardLife={4}
-            cardAttack={2}
-            enemyAttack={5}
-            effect={0}
-            cardName={"Demo card"}
-            cardImage={""}
-            imageDesc={"Img"}
-          />
-          <Card
-            cardLife={2}
-            cardAttack={1}
-            enemyAttack={1}
-            effect={0}
-            cardName={"Demo card"}
-            cardImage={""}
-            imageDesc={"Img"}
-          />
+          <div className='MyCard'>
+            <Card
+              cardLife={10}
+              cardAttack={5}
+              enemyAttack={opponentsAttack}
+              effect={0}
+              cardName={"Demo card"}
+              cardImage={""}
+              imageDesc={"Img"}
+            />
+          </div>
+          <div className='MyCard'>
+            <Card
+              cardLife={8}
+              cardAttack={4}
+              enemyAttack={opponentsAttack}
+              effect={0}
+              cardName={"Demo card"}
+              cardImage={""}
+              imageDesc={"Img"}
+            />
+          </div>
+          <div className='MyCard'>
+            <Card
+              cardLife={12}
+              cardAttack={5}
+              enemyAttack={opponentsAttack}
+              effect={0}
+              cardName={"Demo card"}
+              cardImage={""}
+              imageDesc={"Img"}
+            />
+          </div>
+          <div className='MyCard'>
+            <Card
+              cardLife={4}
+              cardAttack={2}
+              enemyAttack={opponentsAttack}
+              effect={0}
+              cardName={"Demo card"}
+              cardImage={""}
+              imageDesc={"Img"}
+            />
+          </div>
+          <div className='MyCard'>
+            <Card
+              cardLife={2}
+              cardAttack={1}
+              enemyAttack={opponentsAttack}
+              effect={0}
+              cardName={"Demo card"}
+              cardImage={""}
+              imageDesc={"Img"}
+            />
+          </div>
         </div>
 
       </main>
--- memory-card/src/components/Card.js
+++ memory-card/src/components/Card.js
@@ -2,46 +2,17 @@ import React, { useState, useEffect } from "react";
 import "./Card.css";
 
 export default function Card(props) {
-  const [life, setLife] = useState(props.cardLife);
-  const [attack, setAttack] = useState(props.cardAttack);
-
-  useEffect(() => {
-    const setLifeAfterReceivingDamage = (e) => {
-      if (e.target.className === "CardContainer" ||
-        e.target.className === "CardProperties" ||
-        e.target.className === "CardsLife" ||
-        e.target.className === "CardsAttack" ||
-        e.target.className === "CardName") {
-        setLife(life - props.enemyAttack);
-      }
-    }
-    const setAttackAfterEffect = (e) => {
-      if (e.target.className === "CardContainer" ||
-        e.target.className === "CardProperties" ||
-        e.target.className === "CardsLife" ||
-        e.target.className === "CardsAttack" ||
-        e.target.className === "CardName") {
-        setAttack(attack + props.effect);
-      }
-    }
-    document.addEventListener("click", setLifeAfterReceivingDamage);
-    document.addEventListener("click", setAttackAfterEffect);
-    return () => {
-      document.removeEventListener("click", setAttackAfterEffect);
-      document.removeEventListener("click", setLifeAfterReceivingDamage);
-    };
-  }, [life, attack, props.effect, props.enemyAttack]);
 
   return (
     <div className="CardContainer">
       <div className="CardProperties">
         <div className="CardsLife">
           <div>Life</div>
-          {life}
+          {props.cardLife}
         </div>
         <div className="CardsAttack">
           <div>Attack</div>
-          {attack}
+          {props.cardAttack}
         </div>
       </div>
       <div className="CardName">
