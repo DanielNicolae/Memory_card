84cce77c2ce0ef426e3a3b64a3a58ae902441c6e
DanielNicolae <deadlypoet@protonmail.com>
2021-12-14 17:55:54 +0200

    I styled the table and I positioned the cards on the table.

--- memory-card/src/App.js
+++ memory-card/src/App.js
@@ -28,6 +28,53 @@ function App() {
         </div>
       </header>
       <main>
+        <div className='OpponetsCards'>
+          <Card
+            cardLife={10}
+            cardAttack={5}
+            enemyAttack={5}
+            effect={0}
+            cardName={"Demo card"}
+            cardImage={""}
+            imageDesc={"Img"}
+          />
+          <Card
+            cardLife={8}
+            cardAttack={4}
+            enemyAttack={2}
+            effect={0}
+            cardName={"Demo card"}
+            cardImage={""}
+            imageDesc={"Img"}
+          />
+          <Card
+            cardLife={12}
+            cardAttack={5}
+            enemyAttack={3}
+            effect={0}
+            cardName={"Demo card"}
+            cardImage={""}
+            imageDesc={"Img"}
+          />
+          <Card
+            cardLife={4}
+            cardAttack={2}
+            enemyAttack={5}
+            effect={0}
+            cardName={"Demo card"}
+            cardImage={""}
+            imageDesc={"Img"}
+          />
+          <Card
+            cardLife={2}
+            cardAttack={1}
+            enemyAttack={1}
+            effect={0}
+            cardName={"Demo card"}
+            cardImage={""}
+            imageDesc={"Img"}
+          />
+        </div>
         <div className='Table'>
           <div className='OpponentsCardsOnTable'></div>
           <div className='MyCardsOnTable'></div>
